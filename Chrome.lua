3dc95553624389f8dd94983073beae11ffcd5fa6
GregoryLand <landjgregory@gmail.com>
2013-11-29 19:09:46 -0500

    Added chrome.lua - only starts program right now

---
+++ Chrome.lua
@@ -0,0 +1,28 @@
+--Setup Chrome
+RegisterLoreiProgramName("Chrome");
+
+--Setup Chrome keywords
+--RegisterLoreiProgramCommand("");
+
+------------ Object containing the Functions required by Lorei ------------
+if( chrome == nil) then
+	chrome = {}
+end
+
+chrome.FilePath = "C:\\Program Files (x86)\\Google\\Chrome\\Application\\chrome.exe";
+
+--Functions
+-- ToLaunch
+function chrome.ToLaunch()
+	LaunchProgram(chrome.FilePath);
+end;
+
+function chrome.ToClose()
+	ExitProgram(chrome.FilePath);
+end;
+
+
+
+-- On Speech Receved
+function chrome.OnSpeechReceved( e )
+end; 
