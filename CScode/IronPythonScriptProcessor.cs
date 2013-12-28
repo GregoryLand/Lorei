71f3ea1c6c8c4dbbe1a171a2446afba4dfc33a1d
GregoryLand <landjgregory@gmail.com>
2013-12-27 21:36:06 -0500

    Helloworld From IronPython.

---
+++ CScode/IronPythonScriptProcessor.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using IronPython.Hosting;
+using Microsoft.Scripting.Hosting;
+namespace Lorei
+{
+    class IronPythonScriptProcessor : ScriptProcessor
+    {
+        /************ Constructors ************/
+        public IronPythonScriptProcessor()
+        {
+            // Setup python
+            m_pythonEngine = Python.CreateRuntime();
+            dynamic test = m_pythonEngine.UseFile("Scripts/Test.py");
+            test.Simple();
+            
+        }
+
+        /************ Methods ************/
+        // Script Processor Interface
+        public void ParseSpeech(System.Speech.Recognition.SpeechRecognizedEventArgs e)
+        {
+            //throw new NotImplementedException();
+        }
+
+        /************ Helper Methods ************/
+
+        /************ Data ************/
+        Microsoft.Scripting.Hosting.ScriptRuntime m_pythonEngine;
+    }
+}
---
+++ Scripts/Test.py
@@ -0,0 +1,6 @@
+﻿import clr
+clr.AddReference('System.Windows.Forms')
+import System.Windows.Forms 
+
+def Simple():
+	System.Windows.Forms.MessageBox.Show("Hello World")
