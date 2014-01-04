4007224224b20eca4f56d8e4ce638dd31483e1f8
GregoryLand <landjgregory@gmail.com>
2014-01-04 05:45:25 -0500

    Python scripting is now useful.  Adding log script to keep track of events.  Written in python

--- CScode/IronPythonScriptProcessor.cs
+++ CScode/IronPythonScriptProcessor.cs
@@ -4,8 +4,15 @@ using System.Linq;
 using System.Text;
 using IronPython.Hosting;
 using Microsoft.Scripting.Hosting;
+
 namespace Lorei
 {
+    /* This class handles the IronPython scripting.  Iron Python has access to the entire 
+     * .net framework as well as any api calls we decide to provide to it.
+     * Currently it looks in the script folders for any python files and loads each 
+     * python file into its own Script Scope.  The way this is setup means scripts 
+     * will not interfere with each other.
+     */
     class IronPythonScriptProcessor : ScriptProcessor
     {
         /************ Constructors ************/
@@ -13,30 +20,63 @@ namespace Lorei
         {
             m_owner = p_owner;
 
-            // Setup Assemblies=
+            // Setup Assemblies
+            // Could try catch here but without this mscorlib.dll we are screwed 6 ways till Sunday
             m_pythonEngine.LoadAssembly( System.Reflection.Assembly.Load("mscorlib.dll") );
-            //m_pythonEngine.LoadAssembly(System.Reflection.Assembly.Load("System.dll"));
+            m_pythonEngine.LoadAssembly( typeof(System.Speech.Recognition.SpeechRecognizedEventArgs).Assembly );
+
+            // Import required items
+            //m_pythonEngine.ImportModule("System.Speech");
 
             // Setup python
-            m_pythonEngine.Globals.SetVariable("LoreiApi", m_owner);
-            m_pythonEngine.ExecuteFile("Scripts/Test.py");
-            test = m_pythonEngine.ExecuteFile("Scripts/Test.py");//UseFile("Scripts/Test.py");
+            m_pythonEngine.Globals.SetVariable("LoreiApi", m_owner);  // This passes Lorei to the python world
+
+            this.ExecuteEachScript();
         }
 
         /************ Methods ************/
         // Script Processor Interface
         public void ParseSpeech(System.Speech.Recognition.SpeechRecognizedEventArgs e)
         {
-            //throw new NotImplementedException();
-            test.Simple();
+            // Run threw all scripts and call the parse speech function on each one.
+            foreach (dynamic x in m_listOfScopes)
+            {
+                if (x.ContainsVariable("ParseSpeech"))
+                {
+                    // TODO: At some point we will have to go threw and see what exceptions can happen here
+                    // for now just catch them and rethrow.
+                    try
+                    {
+                        x.ParseSpeech(e);
+                    }
+                    catch (Exception oCrap)
+                    {
+                        throw oCrap;
+                    }
+                }
+            }
         }
 
         /************ Helper Methods ************/
+        public void ExecuteEachScript()
+        {
+            String[] scripts = System.IO.Directory.GetFiles("Scripts/");
+
+            foreach (String script in scripts)
+            {
+                if (script.EndsWith(".py"))
+                {
+                    m_listOfScopes.Add( m_pythonEngine.ExecuteFile(script) );
+                }
+            }
+        }
 
         /************ Data ************/
+        // Python Scripting Info
         Microsoft.Scripting.Hosting.ScriptRuntime m_pythonEngine = Python.CreateRuntime();
-        //Microsoft.Scripting.Hosting.ScriptEngine m_pythonEngine = Python.CreateEngine();
+        List<dynamic> m_listOfScopes = new List<dynamic>();
+
+        // Language Processor Info
         public LoreiLanguageProcesser m_owner;
-        dynamic test;
     }
 }
---
+++ Scripts/LogCommands.py
@@ -0,0 +1,22 @@
+﻿import clr
+import LoreiApi
+
+def ParseSpeech(e) :
+    ParseSpeech.counter += 1
+
+    # Open the log file
+    logfile = open('logfile.txt', 'a')
+
+    # Print Header if we just started running
+    if ParseSpeech.counter == 1:
+        logfile.write( '-----------------------Lorei Started -----------------------\n' )
+   
+    # Write out all speech events
+    logfile.write( e.Result.Text )
+    logfile.write( '\n' )
+
+    # close logfile and and set flag so we dont put header in again
+    logfile.close()
+    FirstLoad = 'false';
+
+ParseSpeech.counter = 0
