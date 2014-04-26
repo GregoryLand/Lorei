21749bb805e89e5814cda9aecc424b10ed426737
SkySom <skylar.som@gmail.com>
2014-04-26 11:59:12 -0400

    Deleted everything in Mono-Port to ready to merge WPF in
    Wanted to "pulling" from the most recent branch.

--- README.md
+++
@@ -1,4 +0,0 @@
-Lorie
-=====
-
-Lorie
--- Scripts/Test.py
+++
@@ -1,9 +0,0 @@
-﻿import clr
-clr.AddReference('System.Windows.Forms')
-import System.Windows.Forms
-import LoreiApi
-import TextToSpeechApi
-
-#System.Windows.Forms.MessageBox.Show("Hello World")
-#TextToSpeechApi.SayMessage("Hello From Python")
-    
