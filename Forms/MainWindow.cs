cea982faae6d04534a980b30b0d86877f59d9ad6
SkySom <skylar.som@gmail.com>
2014-05-02 10:44:10 -0400

    Added items to the user interface
    EnableButton, StateText, and Command text have been added. Still need to
    properly place them. Also trying to find something akin to the framed
    panels.

--- Forms/MainWindow.cs
+++ Forms/MainWindow.cs
@@ -4,16 +4,24 @@ namespace Lorei.Forms
 {
     class MainWindow : Window
     {
-        public MainWindow() : base("Language Operated Request and Execution Interface")
+        public MainWindow() : base("L.O.R.E.I.")
         {
-            SetDefaultSize(500, 200);
+            SetDefaultSize(300, 150);
             SetPosition(WindowPosition.Center);
             this.Name = "Main Window";
 
-            Button EnableButton = new Button();
-            EnableButton.
+            Fixed positions = new Fixed();
 
+            Label LastCommand = new Label("Fuck you");
+            Label StateText = new Label("Enabled");
+            Button EnableButton = new Button("Enable/Disable");
 
+            EnableButton.SetSizeRequest(150, 75);
+
+            positions.Put(StateText, 50, 55);
+            positions.Put(EnableButton, 140, 10);
+            positions.Put(LastCommand, 20, 100);
+            Add(positions);
             DeleteEvent += delegate { Application.Quit(); };
         
             ShowAll();    
